5035973c16a1273afb8035647bb6870a61020d33
marija117 <kuveljic117@gmail.com>
2022-08-10 08:16:15 +0200

    Implement delete  method for post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -34,6 +34,9 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   private
