bfdf1d8eca32830ae2b52304ee3e77f995064ced
marija117 <kuveljic117@gmail.com>
2022-08-11 12:25:40 +0200

    Install importmap and bootstrap.

---
+++ app/javascript/application.js
@@ -0,0 +1,6 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// inside app/javascript/application.js  
+// Entry point for the build script in your package.json  
+import "@hotwired/turbo-rails"  
+import "./controllers"  
+import 'bootstrap'
