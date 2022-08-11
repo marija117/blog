9844c16fcff0c12b2b6c0c987e1eb46d10bd6f3e
marija117 <kuveljic117@gmail.com>
2022-08-11 13:13:54 +0200

    Implement created method for comment.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :get_post
+
   def index
   end
 
@@ -9,6 +12,14 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      redirect_to post_path(@post), notice: 'Book was successfully created.'
+    else
+      render json: { errors: @comment.errors }, status: 422 
+    end
   end
 
   def update
@@ -18,5 +29,15 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def get_post
+    @post = Post.find(params[:post_id]) if params[:post_id]
+  end
+
+  def comment_params
+    params.require(:comment).permit(:post_id, :body, :user_id)
   end
 end
