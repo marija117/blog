52b36c310d0cc07a32aef07d9080678309fd7180
marija117 <kuveljic117@gmail.com>
2022-08-12 05:27:04 +0200

    Add routes for reaction resource.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   root "posts#index"
   resources :posts do
-    resources :comments
+    resources :comments do
+      resources :reactions
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
