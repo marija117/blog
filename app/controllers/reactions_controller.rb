28ca1693e3d0d19cdc2aeb57494117a6d762c9b7
marija117 <kuveljic117@gmail.com>
2022-08-12 05:25:30 +0200

    Implement created method for reaction.

---
+++ app/controllers/reactions_controller.rb
@@ -0,0 +1,37 @@
+class ReactionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :get_comment
+  before_action :set_reaction, only: [:destroy]
+
+  def new
+    @reaction = @comment.reactions.build
+  end
+
+  def create
+    @reaction = @comment.reactions.build(reaction_params)
+    @reaction.user_id = current_user.id
+
+    if @reaction.save
+      redirect_to post_comment_path(@comment.post, @comment), notice: 'Reaction was successfully created.'
+    else
+      render json: { errors: @reaction.errors }, status: 422 
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def get_comment
+    @comment = Comment.find(params[:comment_id]) if params[:comment_id]
+  end
+
+  def set_reaction
+    @reaction = @comment.reactions.find(params[:id]) if params[:id]
+  end
+
+  def reaction_params
+    params.require(:reaction).permit(:comment_id, :category, :user_id)
+  end
+end
