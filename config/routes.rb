bd200b879cf0e5d250e7568e88e2d415fd1a1f40
marija117 <kuveljic117@gmail.com>
2022-08-12 10:59:37 +0200

    Modify routes for nested resouces.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root "posts#index"
-  resources :posts do
-    resources :comments do
-      resources :reactions
+  root "users#index"
+  
+  resources :users do
+    resources :posts do
+      resources :comments do
+        resources :reactions
+      end
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
