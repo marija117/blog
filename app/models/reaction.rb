d9160e5bfde2f525fcacee93e4057e34e0cd2935
marija117 <kuveljic117@gmail.com>
2022-08-12 05:29:10 +0200

    Add validation and relations to models.

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,4 +1,11 @@
 class Reaction < ApplicationRecord
   belongs_to :comment
   belongs_to :user
+
+  VALID_CATEGORIES = ['like', 'smile', 'thumbs_up']
+
+  validates :category, presence: true, inclusion: { in: VALID_CATEGORIES }
+  validates :user_id, uniqueness: { scope: [:comment_id]}
+
+  scope :count_by_category, -> (category) { where(category: category).count }
 end
