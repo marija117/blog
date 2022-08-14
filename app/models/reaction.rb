61a4d90cabce84fd8cf14b9a849429ebca34aa57
marija117 <kuveljic117@gmail.com>
2022-08-15 00:36:03 +0200

    Implement functionality of real-time update when creating new reaction.

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -10,6 +10,9 @@ class Reaction < ApplicationRecord
   scope :count_by_category, -> (category) { where(category: category).count }
   scope :user_already_reacted, -> (user) { where user: user}
 
+  after_create_commit { broadcast_append_to self.comment }
+  after_destroy_commit { broadcast_replace_to self.comment }
+
   def self.for_user(user, comment)
     find_by(user_id: user.id, comment_id: comment.id)
   end
