1efa2927f2db774feb94c76f801afba9c0ef9d15
marija117 <kuveljic117@gmail.com>
2022-08-15 00:35:26 +0200

    Implement functionality of real-time update when creating new comment.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,12 @@ class Comment < ApplicationRecord
   has_many :reactions, dependent: :destroy
 
   validates :body, presence: true
+
+  scope :count_by_category, -> (category) { joins(:reactions).where("reactions.category": category).count}
+
+  after_create_commit { broadcast_append_to self.post }
+
+  def count_by_category(category)
+    Comment.joins(:reactions).where("reactions.category": category).count
+  end
 end
