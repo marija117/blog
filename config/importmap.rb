666ef6b48afb444666ccae26a04d081aeba93794
marija117 <kuveljic117@gmail.com>
2022-08-15 00:32:17 +0200

    Install  redis and configure it.

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "jquery_validate", to: "cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/â
 pin "popper", to: "popper.js", preload: true
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.0/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.5/lib/index.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
